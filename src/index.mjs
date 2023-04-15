bc6f6339f1633ba6a9033bbf4d8a2c122e3a59ef
taevel02 <taevel02@gmail.com>
2023-04-15 17:17:26 +0900

    feat: schedule no jobs on market holidays

--- src/index.mjs
+++ src/index.mjs
@@ -5,19 +5,24 @@ import parser from 'koa-bodyparser'
 import dayjs from 'dayjs'
 import { Range, RecurrenceRule, scheduleJob } from 'node-schedule'
 import { makeNote, noteStore } from './evernote.mjs'
-import { dailyReview } from './create-evernote/daily-review.mjs'
+import { dailyReview } from './tools/daily-review.mjs'
+import checkHolidays from './utils/check-holidays.mjs'
 
 const PORT = 8000
+
 const SCHEDULE_RULE = new RecurrenceRule()
 SCHEDULE_RULE.dayOfWeek = [new Range(1, 5)] // Mon - Fri
 SCHEDULE_RULE.hour = 15
-SCHEDULE_RULE.minute = 50
+SCHEDULE_RULE.minute = 40
 SCHEDULE_RULE.tz = 'Asia/Seoul'
 
 scheduleJob(SCHEDULE_RULE, async () => {
-  const title = dayjs(new Date()).format('YYYY년 M월 D일')
+  const today = new Date()
+  if (checkHolidays(today)) return
+
+  const title = dayjs(today).format('YYYY년 M월 D일')
   const noteBody = await dailyReview()
-  makeNote(noteStore, title, noteBody)
+  makeNote(noteStore, title, noteBody, '241a0219-4915-4708-abd4-94109dc4e352')
 })
 
 new Koa()
---
+++ src/utils/check-holidays.mjs
@@ -0,0 +1,27 @@
+import dayjs from 'dayjs'
+
+// 국내 증시 휴장일 2023 (주말 제외)
+export const holidays2023 = [
+  '2023-01-23',
+  '2023-01-24',
+  '2023-03-01',
+  '2023-05-01',
+  '2023-05-05',
+  '2023-05-29',
+  '2023-06-06',
+  '2023-08-15',
+  '2023-09-28',
+  '2023-09-29',
+  '2023-10-03',
+  '2023-10-09',
+  '2023-12-25',
+  '2023-12-29'
+]
+
+const checkHolidays = (date) => {
+  if (holidays2023.inclues(dayjs(new Date(date)).format('YYYY-MM-DD')))
+    return true
+  return false
+}
+
+export default checkHolidays
