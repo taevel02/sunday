5cfd8eb390926b388477c3b5b4c72ebe386f6481
Taehoon Kwon <taevel02@gmail.com>
2023-09-09 23:57:18 +0000

    feat: update stock report templete

--- src/templates/stock-report.ts
+++ src/templates/stock-report.ts
@@ -3,34 +3,40 @@ import getPriceFormatter from '../utils/price-formatter'
 
 // stotprice: 시총
 const StockReport = (stotprice: number) => {
-  return `<b><span>◎ 기본 정보 (${dayjs(new Date()).format(
-    'YYYY.MM.DD'
-  )})</span></b><br /><span>▶ 시총 ${getPriceFormatter(
+  return `<b><span>◎ 기업정보</span></b><br /><span>▶ 시총 ${getPriceFormatter(
     stotprice
-  )}</span><br /><span>▶ 최대주주 및 특수관계인 지분 %</span><br /><span>▶ 별도기준 매출액 연속흑자, 영업이익 연속적자 (적자폭 大/小)</span><br /><span>▶ 부채비율 %, 유보율 %</span><br /><br /><b><span>◎ 재료</span></b><br /><span>▶ #섹터/테마</span><br /><span>- 재료에 대한 설명</span><br /><span>- 관련기사: (날짜)(제목)</span><br /><br /><b><span>◎ 재무</span></b><br /><span>▶ 재무추이(별도)</span><br /><span>▶ 재무비율(연결)</span><br /><br /><hr /><b><span>◎ 종목 히스토리 상세</span></b><br /><span>#섹터/테마</span><br /><span>(날짜)제목</span><br /><span>중요 내용 스크랩</span><br /><br /><b><span>◎ 차트</span></b><br />`
+  )} (${dayjs(new Date()).format(
+    'YYYY.MM.DD'
+  )})</span><br /><span>▶ 최대주주 및 특수관계인 지분 %</span><br /><span>▶ 재무추이</span><br /><span>- ${
+    Number(dayjs(new Date()).format('YYYY')) - 1
+  }년 매출액 억. 영업이익 억.</span><br /><span>- ${
+    Number(dayjs(new Date()).format('YYYY')) - 2
+  }년 매출액 억. 영업이익 억.</span><br /><span>▶ 부채비율 %, 유보율 % (년 월 기준)</span><br /><span>▶ 전자공시: </span><br /><span>▶ 홈페이지: </span><br /><br /><b><span>◎ 재료</span></b><br /><span>▶ 주요 연혁</span><br /><span>▶ 주요 제품</span><br /><span>▶ 연구개발</span><br /><span>▶ #섹터/테마</span><br /><span>▶ 타법인출자 현황</span><br /><span>[관계기업]</span><br /><span>[기타]</span><br /><br /><hr /><b><span>◎ 관련기사</span></b><br /><span>#섹터/테마</span><br /><span>(날짜)제목</span><br /><span>중요 내용 스크랩</span><br /><br /><b><span>◎기타 참고사항</span></b><br /><span>#섹터/테마</span><br /><span>(날짜)제목</span><br /><span>중요 내용 스크랩</span><br />`
 }
 
 export default StockReport
 
-// ◎ 기본 정보 (yyyy.mm.dd)
-// ▶ 시총 억, 유통주식수 만
+// ◎ 기업정보
+// ▶ 시총 억 (yyyy.mm.dd)
 // ▶ 최대주주 및 특수관계인 지분 %
-// ▶ 별도기준 매출액 연속흑자, 영업이익 연속적자 (적자폭 大/小)
-// ▶ 부채비율 %, 유보율 %
+// ▶ 재무추이
+// - (yyyy-1)년 매출액 억. 영업이익 억.
+// - (yyyy-2)년 매출액 억. 영업이익 억.
+// ▶ 부채비율 %, 유보율 % (년 월 기준)
+// ▶ 전자공시:
+// ▶ 홈페이지:
 
 // ◎ 재료
+// ▶ 주요 연혁
+// ▶ 주요 제품
+// ▶ 연구개발
 // ▶ #섹터/테마
-// - 재료에 대한 설명
-// - 관련기사: (날짜)제목
-
-// ◎ 재무
-// ▶ 재무추이(별도)
-// ▶ 재무비율(연결)
+// ▶ 타법인출자 현황
+// [관계기업]
+// [기타]
 
 // ---
-// ◎ 종목 히스토리 상세
+// ◎ 관련기사
 // #섹터/테마
 // (날짜)제목
 // 중요 내용 스크랩
-
-// ◎ 차트
