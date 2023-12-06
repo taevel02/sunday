037892ad0713f2d90816b2ee1574fadbb509826b
taevel02 <taevel02@gmail.com>
2023-12-06 17:36:40 +0900

    fix: & error

--- src/tools/templates.ts
+++ src/tools/templates.ts
@@ -7,19 +7,15 @@ import { formatTradeVolume } from '../tools/formatter'
  * @returns ex. ●삼성전자 (+0.02%)(10000K)
  */
 export const addStockSymbol = (stock: StockInfo) => {
+  const name = stock.ISU_ABBRV.replaceAll(/&/g, '&amp;')
   const rate = parseFloat(stock.FLUC_RT)
+  const volume = formatTradeVolume(stock.ACC_TRDVOL)
 
   if (rate > 0) {
-    return `<b><span style="color: rgb(252, 18, 51);">●${stock.ISU_ABBRV} (+${
-      stock.FLUC_RT
-    }%)(${formatTradeVolume(stock.ACC_TRDVOL)})</span></b>`
+    return `<b><span style="color: rgb(252, 18, 51);">●${name} (+${rate}%)(${volume})</span></b>`
   } else if (rate < 0) {
-    return `<b><span style="color: rgb(13, 58, 153);">●${stock.ISU_ABBRV} (${
-      stock.FLUC_RT
-    }%)(${formatTradeVolume(stock.ACC_TRDVOL)})</span></b>`
+    return `<b><span style="color: rgb(13, 58, 153);">●${name} (${rate}%)($volume})</span></b>`
   } else {
-    return `<b><span>●${stock.ISU_ABBRV} (${formatTradeVolume(
-      stock.ACC_TRDVOL
-    )})</span></b>`
+    return `<b><span>●${name} (${volume})</span></b>`
   }
 }
