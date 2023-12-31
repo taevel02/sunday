4b323fb858bf302e8a53443e7ab89ad6d3e994cc
taevel02 <taevel02@gmail.com>
2023-12-31 18:04:31 +0900

    build(cd): troubleshooting deployment issues with Prisma

--- scripts/after-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-REPOSITORY=/home/ec2-user/sunday
-cd $REPOSITORY
-sudo ~/.local/share/pnpm/pm2 stop sunday
-sudo ~/.local/share/pnpm/pm2 delete sunday
-sudo ~/.local/share/pnpm/pnpm install
-sudo ~/.local/share/pnpm/pm2 start dist/src/index.js --name sunday 
